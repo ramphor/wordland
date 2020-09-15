302d50bb588a87664022426c05ae8df40a88a524
Puleeno Nguyen <puleeno@gmail.com>
2020-09-15 12:32:37 +0700

    Support WordLand template

---
+++ src/WordLand/Template.php
@@ -0,0 +1,29 @@
+<?php
+namespace WordLand;
+
+use Jankx\Template\Template as TemplateLib;
+
+class Template {
+    protected static $loader;
+
+    public static function getLoader() {
+        if (is_null(static::$loader)) {
+            $templateDir = sprintf('%s/templates', dirname(WORDLAND_PLUGIN_FILE));
+            static::$loader = TemplateLib::getLoader(
+                $templateDir,
+                apply_filters('wordland_template_directory_name', 'wordland'),
+                apply_filters('wordland_template_engine', 'wordpress')
+            );
+        }
+
+        return static::$loader;
+    }
+
+    public static function render() {
+        $args = func_get_args();
+        return call_user_func_array(
+            array(static::getLoader(), 'render'),
+            $args
+        );
+    }
+}
--- src/wordland-functions.php
+++ src/wordland-functions.php
@@ -1,5 +1,13 @@
 <?php
+use WordLand\Template;
 
-function wordland_template()
+function wordland_template($templates, $data = array(), $context = null, $echo = true)
 {
+    // Return the WordLand template render output or echo it
+    return Template::render(
+        $templates,
+        $data,
+        $context,
+        $echo
+    );
 }
