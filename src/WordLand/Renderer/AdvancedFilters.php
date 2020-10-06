a1218a8d23b91b636cbcc7870733669a3194ed40
Puleeno Nguyen <puleeno@gmail.com>
2020-10-07 00:03:54 +0700

    Create advanced filters renderer

--- src/WordLand/Renderer/AdvancedFilters.php
+++ src/WordLand/Renderer/AdvancedFilters.php
@@ -5,4 +5,12 @@ use WordLand\Abstracts\Renderer;
 
 class AdvancedFilters extends Renderer
 {
+    public function get_content() {
+        $pre = apply_filters('wordland_pre_render_advanced_filters', null);
+        if (!is_null($pre)) {
+            return $pre;
+        }
+
+        echo 'advanced search';
+    }
 }
