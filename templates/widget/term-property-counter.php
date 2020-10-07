a993d860e99e88c25920edab2bf64f6b0d6fee6e
Puleeno Nguyen <puleeno@gmail.com>
2020-10-08 03:37:24 +0700

    Fix attribute name

--- templates/widget/term-property-counter.php
+++ templates/widget/term-property-counter.php
@@ -1,6 +1,6 @@
 <div class="wordland-property-counter" style="height: <?php echo $height; ?>px;">
     <div class="header">
-        <a href="<?php echo get_term_link($term); ?>" tite="<?php echo $header; ?>"><?php echo $header; ?></a>
+        <a href="<?php echo get_term_link($term); ?>" title="<?php echo $header; ?>"><?php echo $header; ?></a>
     </div>
     <div class="desc"><?php echo $description; ?></div>
     <div class="counter"><?php echo $count; ?></div>
