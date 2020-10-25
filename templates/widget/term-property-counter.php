4a71f948da3b43bce36d1f5981667835f6fa4fbd
Puleeno Nguyen <puleeno@gmail.com>
2020-10-26 06:34:08 +0700

    update templates

--- templates/widget/term-property-counter.php
+++ templates/widget/term-property-counter.php
@@ -1,7 +1,7 @@
 <div class="wordland-property-counter" style="height: <?php echo $height; ?>px;">
-    <div class="header">
+    <h3 class="header">
         <a href="<?php echo get_term_link($term); ?>" title="<?php echo $header; ?>"><?php echo $header; ?></a>
-    </div>
-    <div class="desc"><?php echo $description; ?></div>
+    </h3>
+    <?php if($description): ?><div class="desc"><?php echo $description; ?></div><?php endif; ?>
     <div class="counter"><?php echo $count; ?></div>
 </div>
