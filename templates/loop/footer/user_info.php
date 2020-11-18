1b8183b0852d4a23ea95c672ac54027096e57234
Puleeno Nguyen <puleeno@gmail.com>
2020-11-18 12:08:28 +0700

    Fix user avatar warning

--- templates/loop/footer/user_info.php
+++ templates/loop/footer/user_info.php
@@ -1,6 +1,6 @@
 <div class="user-info">
 	<div class="user-image">
-		<?php echo get_avatar($user->ID, 32) ?>
+		<?php echo get_avatar($user ? $user->ID : 0, 32) ?>
 	</div>
 	<?php echo get_the_author_posts_link(); ?>
 </div>
