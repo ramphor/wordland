db393b7990a9dfb11a0038440753db52f5f86b1b
Puleeno Nguyen <puleeno@gmail.com>
2021-03-21 21:08:46 +0700

    Added wallery to metabox

--- includes/WordLand/Admin/Admin.php
+++ includes/WordLand/Admin/Admin.php
@@ -1,11 +1,16 @@
 <?php
 namespace WordLand\Admin;
 
+use Ramphor\Wallery\Wallery;
+use Ramphor\Wallery\Factory\MetaboxFactory;
 use WordLand\Admin\Property\MetaBox\PropertyInformation;
 use WordLand\Admin\Menu\Dashboard;
+use WordLand\PostTypes;
 
 class Admin
 {
+    protected $wallery;
+
     public function __construct()
     {
         $this->initHooks();
@@ -17,11 +22,29 @@ class Admin
 
         $dashboard = new Dashboard();
         add_action('admin_menu', array($dashboard, 'registerDashboard'));
+
+        add_action('add_meta_boxes', array($this, 'registerGalleryMetabox'));
+
+        if ( class_exists( Wallery::class ) ) {
+            $walleryFactory  = new MetaboxFactory(__('Property Images', 'wordland'));
+            $this->wallery = new Wallery( $walleryFactory );
+
+            $this->wallery->setId( 'wordland_property_gallery_images' );
+        }
     }
 
     public function initProperty()
     {
         $propertyMetabox = new PropertyInformation();
         add_action('add_meta_boxes', array($propertyMetabox, 'registerMetaboxes'));
+    }
+
+    public function registerGalleryMetabox() {
+        add_meta_box(
+            'wordland-property-gallery',
+            __( 'Property Gallery', 'wordland' ),
+            array( $this->wallery, 'render' ),
+            PostTypes::get()
+        );
     }
 }
