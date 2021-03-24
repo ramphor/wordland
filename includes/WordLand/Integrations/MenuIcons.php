88b42673b90a8bde45796d7a57c3e9d302bc7cfc
Puleeno Nguyen <puleeno@gmail.com>
2021-03-24 09:16:15 +0700

    Create integration with menu-icons plugin class

---
+++ includes/WordLand/Integrations/MenuIcons.php
@@ -0,0 +1,11 @@
+<?php
+namespace WordLand\Integrations;
+
+/**
+ * class \WordLand\Integrations\MenuIcons
+ *
+ * This class register WordLand icons to Menu Icons plugin
+ */
+class MenuIcons
+{
+}
