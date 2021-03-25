2aba48d38997634f589917688e519bade476643f
Puleeno Nguyen <puleeno@gmail.com>
2021-03-25 23:13:02 +0700

    Create templates for features

---
+++ templates/agent/my-profile/sections/user-details.php
@@ -0,0 +1,27 @@
+<div class="section-header">
+    <h2 class="section-header_text">
+        <span><?php echo $heading_text; ?></span>
+    </h2>
+</div>
+<div class="section-content profile__settings">
+    <p class="section-description"><?php echo $description; ?></p>
+
+    <div class="agent-position">
+        <div class="edit-display-name">
+            <div class="edit-field display-name">
+                <label for="wordland-agent-position" class="field-label">
+                    <?php echo esc_html(__('Title/Position', 'wordland')); ?>
+                </label>
+                <div class="input-wrap">
+                    <input
+                        id="wordland-agent-position"
+                        type="text"
+                        class="field-editing"
+                        name="display_name"
+                        value=""
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
