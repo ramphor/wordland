18bbfa107aca74e5134c733acc4dde92bb272def
Puleeno Nguyen <puleeno@gmail.com>
2021-03-27 15:49:48 +0700

    Change templates and init new modules

--- includes/WordLand/Frontend/Dashboard/Messages.php
+++ includes/WordLand/Frontend/Dashboard/Messages.php
@@ -1,8 +1,10 @@
 <?php
 namespace WordLand\Frontend\Dashboard;
 
+use WP_Query;
 use Ramphor\User\Abstracts\MyProfileAbstract;
 use WordLand\Template;
+use WordLand\PostTypes;
 
 class Messages extends MyProfileAbstract
 {
@@ -24,11 +26,45 @@ class Messages extends MyProfileAbstract
         );
     }
 
+    public static function joinMesageReferences($join)
+    {
+        return $join;
+    }
+
+    public static function filterAgentMessages($where)
+    {
+        return $where;
+    }
+
+    public static function get_messages($user_id, $page = 1) {
+        add_filter('posts_join', array(__CLASS__, 'joinMesageReferences'));
+        add_filter('posts_where', array(__CLASS__, 'filterAgentMessages'));
+
+        $messages = new WP_Query(array(
+            'post_type' => PostTypes::AGENT_MESSAGE_POST_TYPE,
+            'posts_per_page' => 10,
+            'paged' => $page
+        ));
+
+        remove_filter('posts_where', array(__CLASS__, 'filterAgentMessages'));
+        remove_filter('posts_join', array(__CLASS__, 'joinMesageReferences'));
+
+        return $messages;
+    }
+
     public function render()
     {
+        $currentUser = wp_get_current_user();
+        $current_page = get_query_var('paged');
+        if ($current_page <= 0) {
+            $current_page = 1;
+        }
+
         return Template::render(
             'agent/my-profile/features/messages',
-            array(),
+            array(
+                'messages' => static::get_messages($currentUser->ID, $current_page),
+            ),
             null,
             false
         );
