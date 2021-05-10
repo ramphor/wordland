d74613da1086703070721aa22f122d3ee7911756
Puleeno Nguyen <puleeno@gmail.com>
2021-05-10 18:48:39 +0700

    Support customize for advanced filters renderer

--- includes/WordLand/Renderer/AdvancedFilters.php
+++ includes/WordLand/Renderer/AdvancedFilters.php
@@ -5,9 +5,27 @@ use WordLand\Abstracts\Renderer;
 
 class AdvancedFilters extends Renderer
 {
+    const RENDERER_NAME = 'advanced_filters';
+
+    protected $props = array(
+        'map_only' => false,
+    );
+
+    public function get_name()
+    {
+        return static::RENDERER_NAME;
+    }
+
     public function get_content()
     {
-        $pre = apply_filters('wordland_pre_render_advanced_filters', null);
+        $pre = apply_filters_ref_array(
+            'wordland_pre_render_advanced_filters',
+            array(
+                null,
+                &$this
+            )
+        );
+
         if (!is_null($pre)) {
             return $pre;
         }
