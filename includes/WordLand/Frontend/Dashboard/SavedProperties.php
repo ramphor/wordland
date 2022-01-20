e911b28c1c8eafc8963b65cddf1ecbaf07cf6d92
KILALA_Kiet <kiet.la@kilala.vn>
2022-01-20 07:58:21 +0700

    update save_properties

--- includes/WordLand/Frontend/Dashboard/SavedProperties.php
+++ includes/WordLand/Frontend/Dashboard/SavedProperties.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace WordLand\Frontend\Dashboard;
 
 use Ramphor\User\Abstracts\MyProfileAbstract;
@@ -24,11 +25,39 @@ class SavedProperties extends MyProfileAbstract
         );
     }
 
+    public static function get_saved_properties()
+    {
+        global $wpdb;
+        $searches_per_page = 10;
+        $current_page = get_query_var('paged') ? get_query_var('paged') : 1;
+        $offset = 0;
+        if ($current_page > 1) {
+            $offset = $searches_per_page * ($current_page - 1);
+        }
+
+        $sql = $wpdb->prepare(
+            "SELECT post_id FROM {$wpdb->prefix}ramphor_collection_global g WHERE g.user_id=%d AND collection=%s  ORDER BY added_at DESC LIMIT %d OFFSET %d",
+            get_current_user_id(),
+            "favorite_property",
+            $searches_per_page,
+            $offset
+        );
+
+        $properties = $wpdb->get_results($sql);
+        if (empty($properties)) {
+            return array();
+        }
+        return $properties;
+    }
+
     public function render()
     {
+        $saved_properties = static::get_saved_properties();
         return Template::render(
             'agent/my-profile/features/saved-properties',
-            array(),
+            array(
+                'saved_properties' => $saved_properties,
+            ),
             null,
             false
         );
